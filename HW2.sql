115519a906a4423c862bc35ee692640ae3e97b87
sandeepjohnr <sandeepjohnrofficial@gmail.com>
2026-02-19 19:20:37 +0530

    HW2

--- HW2.sql
+++ HW2.sql
@@ -3,7 +3,7 @@
 use emp1;
 
 -- WAQTD NAME OF THE EMPLOYEE ALONG WITH THEIR ANNUAL SALARY.
-select ename,sal*12 as "anuel salery"
+select ename,sal*12 as "annual salary"
 from emp;
 
 -- WAQTD ENAME AND JOB FOR ALL THE EMPLOYEE WITH THEIR HALF TERM SALARY.
@@ -15,25 +15,25 @@ select emp.*, sal*12 + 2000 as "with bonus"
 from emp;
 
 -- WAQTD NAME SALARY AND SALARY WITH A HIKE OF 10%.
-select ename,sal, sal+sal*10/100 as "salery with hike"
+select ename,sal, sal+sal*10/100 as "salary with hike"
 from emp;
 
 -- WAQTD NAME AND SALARY WITH DEDUCTION OF 25%.
-select ename,sal-sal*25/100 as "sal afre deduction"
+select ename,sal-sal*25/100 as "sal after deduction"
 from emp;
 
 -- WAQTD NAME AND SALARY WITH MONTHLY HIKE OF 50.
-select ename, sal+50 as "salery with 50 hike"
+select ename, sal+50 as "salary with 50 hike"
 from emp;
 -- WAQTD NAME AND ANNUAL SALARY WITH DEDUCTION OF 10%.
-select ename,sal, sal-sal*10/100 as "salery with 10% dedectuion"
+select ename,sal, sal-sal*10/100 as "salary with 10% dedectuion"
 from emp;
 -- WAQTD TOTAL SALARY GIVEN TO EACH EMPLOYEE (SAL+COMM) .
-select ename,sal, sal+comm as "salery with comm"
+select ename,sal, sal+comm as "salary with comm"
 from emp;
 -- WAQTD DETAILS OF ALL THE EMPLOYEES ALONG WITH ANNUAL SALARY.
-select ename,sal, sal*12 as "annual sal"
+select ename,sal, sal*12 as "annual salary"
 from emp;
 -- WAQTD NAME AND DESIGNATION ALONG WITH 100 PENALTY IN SALARY.
-select ename,job, sal-100 as "salery 100 penalty"
+select ename,job, sal-100 as "salary 100 penalty"
 from emp;
